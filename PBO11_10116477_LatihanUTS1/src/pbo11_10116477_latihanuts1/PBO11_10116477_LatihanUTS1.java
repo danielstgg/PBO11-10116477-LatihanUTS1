6e34fbbc09dc6cade4590e928cf327ef0950da1f
stg666 <32459465+stg666@users.noreply.github.com>
2017-11-14 21:27:48 +0700

    Add files via upload

---
+++ PBO11_10116477_LatihanUTS1/src/pbo11_10116477_latihanuts1/PBO11_10116477_LatihanUTS1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116477_latihanuts1;
+
+/**
+ *
+ * @author Fransiskus Xaverius Daniel S
+ */
+public class PBO11_10116477_LatihanUTS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
