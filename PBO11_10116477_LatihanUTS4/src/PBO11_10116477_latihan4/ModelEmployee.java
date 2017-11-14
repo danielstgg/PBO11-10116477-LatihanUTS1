7c897d258c0682add2f2c3e20c9f8546b99b2163
stg666 <32459465+stg666@users.noreply.github.com>
2017-11-14 21:52:33 +0700

    Add files via upload

---
+++ PBO11_10116477_LatihanUTS4/src/PBO11_10116477_latihan4/ModelEmployee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116477_latihan4;
+
+/**
+ *
+ * @author Fransiskus Xaverius Daniel S
+ */
+public class ModelEmployee {
+    
+   float salary =  40000;
+    
+}
